f9c7e5119dcf415b3274ad970cb31e3477f93ab2
JosueBenitez <59493675+JosueBenitez@users.noreply.github.com>
2023-03-07 21:30:01 -0600

    primercomit

--- Proyecto REDES/consultarApi.py
+++
@@ -1,9 +0,0 @@
-import requests
-import datetime
-
-url = "http://192.168.1.48:4040/api/temperaturas"
-payload = {'temperatura': 10, 'humedad': 50, 'fecha': datetime.datetime.now()}
-data = requests.post(url, payload)
-if data.status_code == 200:
-	data = data.json()
-	print(data)
